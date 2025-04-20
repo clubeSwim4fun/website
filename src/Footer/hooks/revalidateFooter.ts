fbb03765587ed4f0feca3ed153d484ea2c20f6a2
Luiz Perez <lgperez.gustavo@gmail.com>
2025-04-20 11:49:24 +0100

    fixed footer and header hooks to take into account locale

--- src/Footer/hooks/revalidateFooter.ts
+++ src/Footer/hooks/revalidateFooter.ts
@@ -2,11 +2,14 @@ import type { GlobalAfterChangeHook } from 'payload'
 
 import { revalidateTag } from 'next/cache'
 
-export const revalidateFooter: GlobalAfterChangeHook = ({ doc, req: { payload, context } }) => {
+export const revalidateFooter: GlobalAfterChangeHook = ({
+  doc,
+  req: { payload, context, locale },
+}) => {
   if (!context.disableRevalidate) {
     payload.logger.info(`Revalidating footer`)
 
-    revalidateTag('global_footer')
+    revalidateTag(`global_footer_${locale}`)
   }
 
   return doc
